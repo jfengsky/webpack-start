b79fc15fd06f937e5748950095b7b4ae0e69b053
姜峰 <jiang.f@Ctrip.com>
2017-05-26 22:33:48 +0800

    save

--- src/Root.js
+++ src/Root.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Root extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'webpack first'
+    }
+  }
+  
   render() {
     return (
       <h1>
-        webpack first
+        {this.state.title}
+        <input type="button" onClick={this.handlerClick} />
       </h1>
     );
+  }
+
+  handlerClick = e => {
+    this.setState({
+      title: 'webpack clicked'
+    });
   }
 }
 
--- test/test.js
+++ test/test.js
@@ -1,18 +1,36 @@
 import React from 'react';
+// import { findDOMNode } from 'react-dom';
+import jsdom from 'jsdom';
 import TestUtils from 'react-addons-test-utils';
 import { expect } from 'chai';
 import App from '../src/Root';
 
+const { JSDOM } = jsdom
+
 function shallowRender(Component) {
   const renderer = TestUtils.createRenderer();
-  renderer.render(<Component/>);
+  renderer.render(<Component />);
   return renderer.getRenderOutput();
 }
 
 describe('Shallow Rendering', function () {
-  it('Root Component test', function () {
+  it('Root Component default test', function () {
     const app = shallowRender(App);
-    expect(app.props.children).to.equal('webpack first');
+    expect(app.props.children[0]).to.equal('webpack first');
     expect(app.type).to.equal('h1');
+  });
+
+  it('Root Component event test', function () {
+
+    if (typeof document === 'undefined') {
+      global.document = new JSDOM('<!doctype html><html><body></body></html>');
+      global.window = document.defaultView;
+      // global.navigator = global.window.navigator;
+    }
+
+    const app = TestUtils.renderIntoDocument(<App />);
+    // const appDOM = findDOMNode(app);
+    // const testButton = appDOM.querySelector('button')
+    // TestUtils.Simulate.click(testButton)
   });
 });
